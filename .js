3d91729cfea0e0c891a0057cff299dc4563d4c82
Ostap Kmyta <54151907+ostapkmyta@users.noreply.github.com>
2019-09-03 22:29:16 +0300

    Create .js

---
+++ .js
@@ -0,0 +1,38 @@
+//https://www.codewars.com/kata/training-js-number-4-basic-data-types-array/train/javascript
+function getLength(arr){
+  
+  return arr.length
+}
+function getFirst(arr){
+
+  return arr[0]
+}
+function getLast(arr){
+  
+  return arr[arr.length-1]
+}
+function pushElement(arr){
+  arr.push(1)
+  
+  return arr;
+}
+function popElement(arr){
+  arr.pop()
+  
+  return arr
+}
+
+//https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript?fbclid=IwAR3o1GORcRVvcvwUoDTJbyIiKiR6EEWPGhNi4BkegWRNVV6jSFNfwt47NWk
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    var count  ;
+ 
+    for (var i=0;i<args.length;i++){
+        if(args[i]<=args[0]){
+            args[0]  = args[i];    
+            count = args[i];
+        }
+      }
+    return count;
+  }
+}
